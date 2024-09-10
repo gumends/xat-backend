1c05581be436b2308ac9cdde3fb31d01417b4f98
gumends <gustavo08.mendes@gmail.com>
2024-09-10 01:29:54 -0300

    bd atual

--- prisma/migrations/20240910041953_/migration.sql
+++ prisma/migrations/20240910041953_/migration.sql
@@ -1,8 +1,23 @@
 -- CreateTable
+CREATE TABLE "usersEntity" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "nome" TEXT NOT NULL,
+    "sobreNome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "avatar" TEXT,
+    "status" INTEGER NOT NULL DEFAULT 0,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
 CREATE TABLE "sessao" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "usuario_id_I" TEXT NOT NULL,
     "usuario_id_II" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
     CONSTRAINT "sessao_usuario_id_I_fkey" FOREIGN KEY ("usuario_id_I") REFERENCES "usersEntity" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "sessao_usuario_id_II_fkey" FOREIGN KEY ("usuario_id_II") REFERENCES "usersEntity" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
@@ -13,6 +28,8 @@ CREATE TABLE "conversas" (
     "texto" TEXT NOT NULL,
     "usuario_id" TEXT NOT NULL,
     "sessao_id" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
     CONSTRAINT "conversas_usuario_id_fkey" FOREIGN KEY ("usuario_id") REFERENCES "usersEntity" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "conversas_sessao_id_fkey" FOREIGN KEY ("sessao_id") REFERENCES "sessao" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
